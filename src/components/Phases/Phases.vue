25f76489c95aaf641227bb0c8df5c08300df3769
Ricardo Vieira Amorim dos Santos <ricardos@spi.local>
2017-12-22 09:49:51 -0200

    T

--- src/components/Phases/Phases.vue
+++ src/components/Phases/Phases.vue
@@ -1,37 +1,161 @@
 <template>
-    <div>        
-        <h1>Nova Atualizado</h1>        
+    <div>
+        <!--               -->
+        <!--               -->
+        <!--               -->
+        <!--Nav de produtos-->
+        <!--               -->
+        <!--               -->
+        <!--               -->                         
         <div class="fixed-top nav-produtos">
-            <form>
-                <ul class="nav">                                  
-                    <li class="nav-item">
-                        <div class="dropdown input-group input-group-sm">  
-                            <label class="mr-sm-2">Nome do produto : </label>                                                                  
-                            <input type="text" v-model="nomeP" @keyup="getResults()" class="dropdown-toggle form-control form-control-sm" data-toggle="dropdown" placeholder="Nome do Produto">
-                            <ul class="dropdown-menu">
-                                <li v-for="(p, index) in produtos" @click.stop.prevent='nomeP=p.productName; recipe.recipeProduct=p;'>{{p.productName}}</li>                                       
-                            </ul>                                                 
-                        </div>                            
+            <form>                
+                <ul class="nav">                
+                    <li class="nav-item ">                    
+                        <label class="mr-sm-2">Nome da receita : </label>
+                        <input type="text" :disabled="ok" class="form-control form-control-sm" v-model="recipe.recipeName" required placeholder="Nome da receita">                                                                                                       
+                    <li class="nav-item">                        
+                        <label class="mr-sm-2">Código : </label>
+                        <input class="form-control form-control-sm" :disabled="ok" required v-model="recipe.recipeCode" placeholder="Código da receita"/>
                     </li>
-                    <li class="nav-item ">
-                        <div class="input-group input-group-sm">
-                            <label class="mr-sm-2">Nome da receita : </label>
-                            <input type="text" class="form-control form-control-sm" placeholder="Nome da receita">                        
-                        </div>    
-                    </li>                                                                                            
-                    <li class="nav-item">
-                        <div class="input-group input-group-sm">
-                            <label class="mr-sm-2">Código : </label>
-                            <input class="form-control form-control-sm"/>
-                        </div>    
-                    </li>                        
-                </ul>             
-            </form>                                                                    
-        </div>            
+                    <li class="nav-item">                        
+                        <button type="button" class="btn btn-success" :disabled="carregando || recipe.recipeName==undefined" @click.stop.prevent="createRecipe()">Enviar</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </li> 
+                    <li class="nav-item justify-content-end">                        
+                        <button type="button" class="btn btn-primary" :disabled="carregando || recipe.recipeName==undefined" @click.stop.prevent="displayCadPhase=true">Cadastrar fase</button>                        
+                    </li> 
+                    <li class="nav-item justify-content-end" v-show="json.stringify(recipeProductEnd) !== '{}'">                        
+                        Produto : {{recipeProduct.productName}} | Quantidade do produto : {{recipeProductEnd.value+''+recipeProductEnd.measurementUnit}} | Tipo do produto : {{recipeProductEnd.phaseProductType}}  
+                    </li>                    
+                </ul>                                                         
+            </form>
+       
+            <!--               -->
+            <!--               -->
+            <!--               -->
+            <!-- Produto Final -->
+            <!--               -->
+            <!--               -->
+            <!--               -->        
+            <div class="fixed-top nav-produtos nav-produto-final" v-show="ok">
+                <div class="input-group input-group-sm">                    
+                    <label class="mr-sm-2">Digite a quantidade : </label>
+                    <input class="form-control form-control-sm"  required v-model="recipeProductEnd.value" placeholder="Valor"/>
+                    <label class="mr-sm-2">Digite a unidade de medida : </label>
+                    <input class="form-control form-control-sm"  required v-model="recipeProductEnd.measurementUnit" placeholder="Ex.: kg"/>
+                    <label class="mr-sm-2">Digite o tipo de produto : </label>
+                    
+                    <select class="form-control form-control-sm" v-model="recipeProductEnd.phaseProductType">                        
+                        <option value="" selected disabled>Campo para busca</option>
+                        <option value="scrap">Rejeito</option>
+                        <option value="finished">Acabado</option>
+                        <option value="semi_finished">Semi-Acabado</option>                   
+                    </select>
+                    <label class="mr-sm-2">Digite o nome do produto : </label>                            
+
+                    <div class="dropdown">   
+                        <input @keyup="recipeProducts=getProductFinal(productName)" v-model="productName" placeholder="Nome do produto" class="btn btn-secondary dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"/>
+                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                             <a class="dropdown-item" @click.stop.prevent="recipeProduct=p; productName=recipeProduct.productName; recipeProducts=[]" v-for="(p,index) in recipeProducts">{{p.productName}}</a>                            
+                        </div>                            
+                        
+                    </div>
+
+                    <button @click.stop.prevent="createRecipeProduct()" :disabled="recipeProduct.productName==undefined">Ola mundo</button>
+                </div><br><br>                
+            </div>
+        </div>
+        <div class="progress fixed-top" v-show="carregando">
+            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+        </div>
+        <div class="row conteudo">
+            <!--                       --> 
+            <!--                       -->
+            <!--                       --> 
+            <!--   Listagem de fase    -->
+            <!--                       -->
+            <!--                       -->
+            <!--                       -->        
+            <div class="listagem col-8">            
+                <div v-for="(pha, index) in phases">                        
+                    <div class="card-body">
+                        Fase {{index}} Nome da fase = {{pha.phaseName}} --- Código da fase {{pha.phaseCode}} <i class="fa fa-check-square-o icon-right" @click.stop.prevent="phase=pha" aria-hidden="true"></i>
+                    </div>                                                                                                                                            
+                </div>
+            </div>
+
+            <!--                       --> 
+            <!--                       -->
+            <!--                       --> 
+            <!--   Cadastro da fase    -->
+            <!--                       -->
+            <!--                       -->
+            <!--                       -->  
+            <div class="cadForm" v-show="displayCadPhase">                                                          
+                <form>
+                    <div class="form-row">
+                        <div class="alert alert-danger form-control" v-show="mensagem!=''" role="alert">{{mensagem}}</div>
+                        <div class="alert alert-success form-control" v-show="mensagemSuc!=''" role="alert">{{mensagemSuc}}</div>                                                                                                                       
+                        <label for="">Nome da fase</label>
+                        <input type="text" class="form-control form-control-sm" v-model="phase.phaseName" placeholder="Nome">                                    
+                        <label for="inputPassword4">Código da fase</label>                            
+                        <input type="text" class="form-control form-control-sm" v-model="phase.phaseCode" placeholder="Código">                    
+                        <div>                                                    
+                            <button class="btn btn-success"><i :disabled="phase==undefined || json.stringify(phase) === '{}'" @click.stop.prevent="(phase.phaseId!=undefined) ? putPhase(phase) : createPhase(phase);" class="fa fa-check-square" aria-hidden="true"></i></button> 
+                            <button class="btn btn-danger"><i @click.stop.prevent="deletePhase(phase)" :disabled="phase.phaseId == undefined" class="fa fa-window-close" aria-hidden="true"></i></button>
+                            <div class="btn btn-primary pull-right" @click.stop.prevent="phase={}">
+                                Limpar
+                            </div>
+                        </div>                                   
+                    </div>                                                                                                                                                       	                                            
+                </form>    
+            </div>  
+        </div>                      
     </div>      
 </template>
 <script src="./js/phases.js">
 </script>
 <style>
-    @import url('./css/phases.css');    
+@import url("./css/phases.css");
 </style> 
+<!--
+{
+  "phaseId": 1,
+  "phaseName": "cois2a",
+  "phaseCode": "xpto",
+  "inputProducts": [],
+  "outputProducts": [],
+  "predecessorPhaseId": 0,
+  "phaseProducts": [3, 2]
+}
+
+
+<div class="col-4">
+            <ul class="nav row">                                                      
+                <li class="nav-item col-md-3">
+                    <div class="form-group ">
+                        <label class="mr-sm-2">Digite o nome do produto : </label>
+                        <input class="dropdown-menu" @keyup="phaseProducts=getProductFinal(pha.name)" placeholder="Nome do produto"/>                            
+                        <a class="dropdown-item" v-for="phaPro in phaseProducts" @click.stop.prevent="">{{phaPro.productName}}</a>                                                                                                                                
+                    </div>    
+                </li>                                                                                            
+                <li class="nav-item col-md-3">
+                    <div class="form-group">
+                        <label class="mr-sm-2">Digite a unidade de medida : </label>value
+                        <input class="form-control form-control-sm" required  placeholder="Ex.: Kg"/>
+                    </div>    
+                </li>
+                <li class="nav-item col-md-2">
+                    <div class="form-group">
+                        <label class="mr-sm-2">Digite o valor : </label>
+                        <input type="text" placeholder="Valor" class="form-control form-control-sm">
+                    </div>    
+                </li>  
+                <li class="nav-item col-md-3">
+                    <div class="form-group">
+                        <label class="mr-sm-2">Digite o tipo do produto : </label>
+                    <input class="form-control form-control-sm" placeholder="Status" required/>
+                    </div>    
+                </li>                                                  
+            </ul> 
+            <button class="form-control btn-success">Enviar produto</button>
+        </div>               -->
--- src/components/Phases/js/phases.js
+++ src/components/Phases/js/phases.js
@@ -4,9 +4,9 @@ es6promisse.polyfill();
 /*
     Example json recipe!
     {
-    "recipeId": 3,
-    "recipeName": "teste de receita",
-    "recipeCode": "5050505050",
+        "recipeId": 3,
+        "recipeName": "teste de receita",
+        "recipeCode": "5050505050",
         "recipeProduct": {
             "phaseProductId": 13,
             "productId": 1,
@@ -19,42 +19,131 @@ es6promisse.polyfill();
             "productGTIN": "+9999999",
             "childrenProductsIds": []
         }
-    },
-    "phasesId": []
+        "phasesId": []
     }
 */
 export default {    
     name: "Phases",
     data(){
-        return {                                               
-            url:'http://brsbap01:8003/api/', 
-            produtos:[],
-            produto: {},        
-            nomeP:'',
-            recipe:{}
+        return {
+            json:JSON, 
+            config : {
+                headers: {'Cache-Control':'no-cache'}
+            },                                              
+            url:'http://brsbap01:8003/api/',                                 
+            ok:false,
+            recipeProduct:{},
+            recipeProductEnd:{},
+            recipeProducts: [],
+            carregando:false,
+            recipe:{},            
+            phase:{},             
+            phases:[],
+            mensagem:'',
+            mensagemSuc:'',
+            productName:'',
+            name:'',            
+            displayCadPhase:false,
+            phaseProduct:{},            
+            phaseProducts: [],                                 
         }
     },  
     computed:{       
 
     },      
-    methods:{                        
-        getResults(){
-            if(this.nomeP.length<3){
-                this.produtos=[];
-                return;
-            }    
-            this.produtos=[];
-            var config = {
-                headers: {'Cache-Control':'no-cache'}
-            };            
-            users: [];
-            axios.get(this.url+"/products?fieldFilter=productName&fieldValue="+this.nomeP,config).then((response)=>{                                 
+    methods:{ 
+        createRecipe(){             
+            this.mensagemSuc='';
+            this.carregando=true;                       
+            axios.post(this.url+"recipes/",this.recipe).then((response)=>{
+                console.log(response.data);
+                this.recipe=response.data;
+                this.ok=true;
+                this.carregando=false;
+            },(error)=>{
+                console.log(error);
+                this.carregando=false;
+            });
+        },  
+
+        createRecipeProduct(){                        
+            this.carregando=true;            
+            this.recipeProductEnd.productId=this.recipeProduct.productId;            
+            axios.post(this.url+"recipes/product/"+this.recipe.recipeId,this.recipeProductEnd).then((response)=>{
+                console.log(response.data);                                
+                this.carregando=false;                                
+                alert('Cadastrado com sucesso');
+                this.displayCadPhase=true;
+            },(error)=>{
+                console.log(error);
+                this.carregando=false;
+            });
+        },
+
+
+
+        createPhase(phase){
+            this.mensagemSuc='';
+            this.carregando=true;  
+            console.log(phase);
+            axios.post(this.url+"phases/",phase).then((response)=>{
+                console.log(response.data);
+                phase=response.data;                              
+                this.relacionaFase(phase); 
+                this.carregando=false;                 
+            },(error)=>{
+                console.log(error);
+                this.carregando=false;
+            });
+        },
+        putPhase(){
+
+        },
+        deletePhase(){
+
+        },   
+                
+        relacionaFase(phase){  
+            this.mensagemSuc='';                                  
+            axios.post(this.url+"recipes/phases/"+this.recipe.recipeId,phase).then((response)=>{
+                console.log(response.data);
+                this.phase.products=[];
+                this.phases.push(this.phase);
+                this.phase = {};                
+                this.mensagemSuc='Fase relacionada com sucesso';
+                this.ok=true;                
+            },(error)=>{
+                console.log(error);
+                this.carregando=false;
+            });
+        },    
+
+
+        getResults(index){
+            if(this.phases[index].productName.length<3){this.phases[index].products=[];return;}    
+            this.phases[index].products=[];                                    
+            axios.get(this.url+"/products?&fieldFilter=productName&fieldValue="+this.phases[index].productName,this.config).then((response)=>{                                           
                 response.data.values.forEach((pro) => {
-                    this.produtos.push(pro);
+                    this.phases[index].products.push(pro);
                 });
             },(error)=>{
                 console.log(error);
             })
+        },
+
+        getProductFinal(name){            
+            var array=[];                          
+            if(name.length<3){return;}                
+            axios.get(this.url+"/products?&fieldFilter=productName&fieldValue="+this.productName,this.config).then((response)=>{                                           
+                response.data.values.forEach((pro) => {
+                    array.push(pro);                    
+                });
+            },(error)=>{
+                console.log(error);
+            })            
+            return array;
         }
     }
+
+ 
 }    
