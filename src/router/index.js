1721a57001c28da30a4cd919cb34534f025e6e1a
Flávia Janine <flaviabeo@hotmail.com>
2017-12-28 16:28:08 -0200

    Cadastro e listagem na tela de ferramentas

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import ThingCreate from '@/components/things/ThingCreate'
 import GenerateProducts from '@/components/Products/GenerateProducts'
 import Phases from '@/components/Phases/Phases'
+import ToolCreate from '@/components/Tools/ToolCreate'
 Vue.use(Router)
 
 export default new Router({
@@ -21,6 +22,11 @@ export default new Router({
             path: '/phases',
             name: 'Phases',
             component: Phases
+        },
+        {
+            path: '/toolCreate',
+            name: 'ToolCreate',
+            component: ToolCreate
         },
     ]
 })
