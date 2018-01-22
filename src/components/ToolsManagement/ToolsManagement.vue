ddc814fe5683072ea69b834d1fb7f9073f756054
Flávia Janine <flaviabeo@hotmail.com>
2018-01-22 11:11:30 -0200

    Ajuste de Layout para produção

--- src/components/ToolsManagement/ToolsManagement.vue
+++ src/components/ToolsManagement/ToolsManagement.vue
@@ -18,7 +18,7 @@
                         <div class="card-header card-header-tools-manag">
                                 <b>Lista de Ferramentas</b>
                             </div>
-                        <div class="table table-striped" v-for="(t,index) in tools" :key="index">
+                        <div v-for="(t,index) in tools" :key="index">
                             <div class="card">
                                 <b></b>
                             </div>
@@ -36,7 +36,7 @@
                                     <b>Status: </b>{{t.status | StatusName}}
                                 </label>
                                 <label class="ls ls20">
-                                    <button type="button" class="btn btn-success btn-sm"  @click="catchToolToChange(t)" data-target="#editarStatus" data-toggle="modal" >
+                                    <button type="button" class="btn btn-success btn-sm"  @click="catchToolToChange(t)">
                                     Mudar Status
                                 </button>
                                 </label>
@@ -45,15 +45,7 @@
                         </div>
                     </div>
                     <!-- Modal -->
-                    <div class="modal fade" id="editarStatus" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Confirmar Alteração do Status</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true" @click="tool = {}; obj = {}">&times;</span>
-                            </button>
-                        </div>
+                     <b-modal ref="modalGerT" hide-footer title="Atualizar Status da Ferramenta">
                         <div class="modal-body">
                             <div class="alert alert-success" role="alert" v-if="tsUpdated">
                                 Status da Ferramenta Alterado com Sucesso !
@@ -102,9 +94,7 @@
                                 <i class="fa fa-check-square" aria-hidden="true"></i>
                             </button>   
                         </div>
-                        </div>
-                    </div>
-                    </div>
+                    </b-modal>
                 </div>
             </div>
         </div>
