294485e2ad6aeb28d914e86b282eab54e2621cf4
Flávia Janine <flaviabeo@hotmail.com>
2018-01-26 14:38:18 -0200

    Gráfico colocado na página de rastreamento

--- src/components/Historian/js/historian.js
+++ src/components/Historian/js/historian.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import axios from '../../../.././node_modules/axios/index.js'
 import es6promisse from '../../../.././node_modules/es6-promise/dist/es6-promise.min.js'
 import { setTimeout } from 'timers'
@@ -8,11 +9,12 @@ import bModal from 'bootstrap-vue/es/components/modal/modal'
 import bModalDirective from 'bootstrap-vue/es/directives/modal/modal'
 import datePicker from 'vue-bootstrap-datetimepicker'
 import VueTimepicker from 'vue2-timepicker'
+import VueTiles from 'vue-tiles'
+import AmCharts from 'amcharts3'
+import AmSerial from 'amcharts3/amcharts/serial'
+import 'vue-tiles/dist/vue-tiles.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css'
-import Vue from 'vue'
-import VueTiles from 'vue-tiles'
-import 'vue-tiles/dist/vue-tiles.css'
 import { LinkTile, ContentSm, ContentMd, ContentLg } from 'vue-tiles'
 
 es6promisse.polyfill();
@@ -74,8 +76,8 @@ export default {
             idstat: '',
             idstatcollpse: '',
             themeArr: ['theme1', 'theme2', 'theme3', 'theme4', 'theme5'],
-            selectedTheme: [],           
-        }
+            selectedTheme: []
+             }
     },
     components: {
         'b-button': bButton,
@@ -86,7 +88,7 @@ export default {
         'b-collapse': bCollapse,
         'b-modal': bModal,
         'date-picker': datePicker,
-        'vue-timepicker': VueTimepicker   
+        'vue-timepicker': VueTimepicker
     },
     directives: {
         'b-toggle': vBToggle,
@@ -94,6 +96,12 @@ export default {
         'b-modal': bModalDirective
     },
     methods: {
+        addData () {
+            this.dataset.push(this.dataentry)
+            this.labels.push(this.datalabel)
+            this.datalabel = ''
+            this.dataentry = ''
+          },
         getThings(){                                  
             axios.get(this.urlThing).then((response)=>{                                           
                 this.things = response.data;
@@ -106,13 +114,12 @@ export default {
             setTimeout(() => {
                 var Ini = this.date.toString()+' '+this.timeIni.HH+':'+this.timeIni.mm;  
                 var ticksI = this.dateToTicks(Ini);
-                var Fim = this.date.toString()+' '+this.timeFim.HH+':'+this.timeFim.mm;
+                var Fim = this.datef.toString()+' '+this.timeFim.HH+':'+this.timeFim.mm;
                 var ticksF = this.dateToTicks(Fim);
                 console.log('I: '+ticksI);
-                console.log('dateI: '+ this.ticksToDate(ticksI));
                 console.log('F: '+ticksF);
-                console.log('dateF: '+ this.ticksToDate(ticksF));
-                axios.get(this.url+'thingId='+this.thingId+'&startDate='+this.ticksI+'&endDate='+this.ticksF).then(response => {
+                console.log(this.url+'thingId='+this.thingId+'&startDate='+ticksI+'&endDate='+ticksF);
+                axios.get(this.url+'thingId='+this.thingId+'&startDate='+ticksI+'&endDate='+ticksF).then(response => {
                     this.history = response.data;
                     console.log(this.history);
                     this.carregando = false;
@@ -135,17 +142,174 @@ export default {
             var ticks = ((date.getTime() * 10000) + 621355968000000000) - (date.getTimezoneOffset() * 600000000);
             return ticks;
         },
+
         showModal() {
             setTimeout(() => {
                 this.$refs.myModalEdit.show()
             }, 200);
         },
         hideModal() {
-            this.$refs.myModalEdit.hide()
-        }
+            this.$refs.myModalEdit.hide();
+            this.created();
+        },
+        created() {
+            window.AmCharts.makeChart("chartdiv",
+            {
+                "type": "serial",
+                "categoryField": "category",
+                "autoMarginOffset": 40,
+                "marginRight": 60,
+                "marginTop": 60,
+                "startDuration": 1,
+                "borderColor": "#C67373",
+                "fontSize": 13,
+                "theme": "light",
+                "categoryAxis": {
+                    "gridPosition": "start"
+                },
+                "trendLines": [],
+                "graphs": [
+                    {
+                        "balloonColor": "#8EE8EB",
+                        "balloonText": "[[title]] of [[category]]:[[value]]",
+                        "bullet": "round",
+                        "bulletSize": 10,
+                        "color": "#000000",
+                        "columnWidth": 0.58,
+                        "fixedColumnWidth": -7,
+                        "fontSize": -4,
+                        "id": "AmGraph-1",
+                        "lineAlpha": 1,
+                        "lineThickness": 3,
+                        "tabIndex": -3,
+                        "title": "graph 1",
+                        "type": "smoothedLine",
+                        "valueField": "set_ll"
+                    },
+                    {
+                        "balloonColor": "#8EE8EB",
+                        "balloonText": "[[title]] of [[category]]:[[value]]",
+                        "bullet": "round",
+                        "bulletSize": 10,
+                        "color": "#000000",
+                        "id": "AmGraph-2",
+                        "lineThickness": 3,
+                        "type": "smoothedLine",
+                        "title": "graph 2",
+                        "valueField": "set_l"
+                    },
+                    {
+                        "balloonColor": "#8EE8EB",
+                        "balloonText": "[[title]] of [[category]]:[[value]]",
+                        "bullet": "round",
+                        "bulletSize": 10,
+                        "color": "#000000",
+                        "id": "AmGraph-3",
+                        "title": "graph 3",
+                        "lineThickness": 3,
+                        "type": "smoothedLine",
+                        "valueField": "set_hh"
+                    },
+                    {
+                        "balloonColor": "#8EE8EB",
+                        "balloonText": "[[title]] of [[category]]:[[value]]",
+                        "bullet": "round",
+                        "bulletSize": 10,
+                        "color": "#000000",
+                        "id": "AmGraph-4",
+                        "title": "graph 4",
+                        "lineThickness": 3,
+                        "type": "smoothedLine",
+                        "valueField": "set_h"
+                    },
+                    {
+                        "balloonColor": "#8EE8EB",
+                        "balloonText": "[[title]] of [[category]]:[[value]]",
+                        "bullet": "round",
+                        "bulletSize": 10,
+                        "color": "#000000",
+                        "id": "AmGraph-5",
+                        "title": "graph 5",
+                        "lineThickness": 3,
+                        "type": "smoothedLine",
+                        "valueField": "medicao"
+                    }
+                ],
+                "guides": [],
+                "valueAxes": [
+                    {
+                        "id": "ValueAxis-1",
+                        "title": ""
+                    }
+                ],
+                "allLabels": [],
+                "balloon": {},
+                "titles": [],
+                "dataProvider": [
+                    {
+                        "category": "category 1",
+                        "set_ll": 8,
+                        "set_l": "35",
+                        "set_hh": 69,
+                        "set_h": 16,
+                        "medicao": 74
+                    },
+                    {
+                        "category": "category 2",
+                        "set_ll": 6,
+                        "set_l": 52,
+                        "set_hh": 69,
+                        "set_h": 61,
+                        "medicao": 13
+                    },
+                    {
+                        "category": "category 3",
+                        "set_ll": 2,
+                        "set_l": 26,
+                        "set_hh": 20,
+                        "set_h": 22,
+                        "medicao": 35
+                    },
+                    {
+                        "category": "category 4",
+                        "set_ll": 1,
+                        "set_l": 10,
+                        "set_hh": 7,
+                        "set_h": 89,
+                        "medicao": 16
+                    },
+                    {
+                        "category": "category 5",
+                        "set_ll": 2,
+                        "set_l": 88,
+                        "set_hh": 30,
+                        "set_h": 87,
+                        "medicao": 96
+                    },
+                    {
+                        "category": "category 6",
+                        "set_ll": 3,
+                        "set_l": 18,
+                        "set_hh": 26,
+                        "set_h": 24,
+                        "medicao": 3
+                    },
+                    {
+                        "category": "category 7",
+                        "set_ll": 6,
+                        "set_l": 3,
+                        "set_hh": 50,
+                        "set_h": 52,
+                        "medicao": 42
+                    }
+                ]
+            }
+            );
+          }
         },
     beforeMount: function() {
         this.showModal();
         this.getThings();
-    }
+    },
+
 }
