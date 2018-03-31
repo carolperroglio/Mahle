334e3eb83fae18bff9f9c9b2d38effa3d31aecb9
Ricardo Vieira Amorim dos Santos <ricardos@spi.local>
2018-03-31 15:48:55 -0300

    Parametros de linha criada

--- src/components/LineParameters/ListLineParameters.vue
+++ src/components/LineParameters/ListLineParameters.vue
@@ -5,7 +5,7 @@
         <!--Redireciona p/página de cad nova -->
         <!--                                 -->
         <!--                                 -->
-        <nav class="fixed-top nav-recipe-liga">            
+        <div class="fixed-top nav-recipe-liga">            
             <ul class="nav d-flex align-items-center">
                 <li class="nav-item col-sm-1.5">
                     <h1 class="title-page-gp-liga"><b>Gerenciamento de Liga</b></h1>
@@ -17,7 +17,7 @@
                 </li>
             </ul>
             <div class="fundo-branco-liga">
-                <div class="cabecalho-table-liga">
+                <!--<div class="cabecalho-table-liga">
                     <label @click.stop.prevent="cabecalhoSetas[0]==false?desorganizar(recipes, 'recipeName',0):organizar(recipes, 'recipeName',0);" class="ls2-cabecalho-liga">
                         <b><font class="cursor-class" color="#ffffff">Nome da Liga &nbsp;&nbsp;&nbsp;
                             <i class="fa fa-sort-desc" v-if="cabecalhoSetas[0]==false" aria-hidden="true"></i>
@@ -38,9 +38,9 @@
                             <i class="fa fa-sort-asc" v-if="cabecalhoSetas[2]==true" aria-hidden="true"></i>
                         </font></b>
                     </label>                
-                </div>
+                </div>-->
             </div>    
-        </nav>
+        </div>
 
         <!--                         -->
         <!--                         -->
@@ -53,25 +53,21 @@
                     <div class="progress-bar progress-bar-striped progress-bar-animated " role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
                     </div>
                 </div>
-                <div v-for="(recipe,index) in recipes" v-bind:class="{cinza: index%2==0}" v-bind:key="index">    
+                <div >    
                     <label class="ls4">
-                        <b><font color="#9BA6A5"> </font></b> {{recipe.recipeName}}
+                        <b><font color="#9BA6A5"> </font></b> {{"recipe.recipeName"}}
                     </label>
                     <label class="ls4">
-                        <b><font color="#9BA6A5"> </font></b>{{recipe.recipeCode}}
+                        <b><font color="#9BA6A5"> </font></b>{{"recipe.recipeCode"}}
                     </label>
                     <label class="ls4">
-                        <b><font color="#9BA6A5"> </font></b>{{recipe.recipeDescription}}
+                        <b><font color="#9BA6A5"> </font></b>{{"recipe.recipeDescription"}}
                     </label>
                     <label class="lsbtn">
-                    <button type="button" class="btn btn-danger btn-edit btn-sm fa fa-trash-o" style="font-size:17px; cursor:pointer" aria-hidden="true" @click.stop.prevent="recipeDelete(recipe.recipeId);">
+                        <button type="button" class="btn btn-danger btn-edit btn-sm fa fa-trash-o" style="font-size:17px; cursor:pointer" aria-hidden="true">
                     </button>
-                    </label>
-                    <router-link :to="{ name: 'Liga',params: { id: recipe.recipeId }}">
-                        <i class="fa fa-edit icon-right2 icon-style" style="font-size:22px; cursor:pointer" @click="id = recipe.recipeId">
-                        </i>
-                    </router-link>
-                    <div class="paginacao-liga" v-show="total>0">
+                    </label>          
+                    <!--<div class="paginacao-liga" v-show="total>0">
                         <nav aria-label="">
                             <ul class="pagination justify-content-center">
                                 <li v-show="startat>0" class="page-item">
@@ -85,7 +81,7 @@
                                 </li>
                             </ul>
                         </nav>
-                    </div>
+                    </div>-->
                 </div>
             </div>
         </div>
--- src/components/Products/GenerateProducts.vue
+++ src/components/Products/GenerateProducts.vue
@@ -1,64 +1,62 @@
 
 <template >
-    <div>
-        
+    <div>        
         <!--                                 -->
         <!--                                 -->
         <!--                                 -->
         <!-- Menu de navegação de produtos   -->
         <!--                                 -->
         <!--                                 -->
-        <!--               Modal             -->
-        
-            <b-modal ref="myModalRef" hide-footer title="Cadastro de Matéria-Prima">                    
-                <form>
-                    <div>
-                        <div class="alert alert-danger form-control" v-show="mensagem!=''" role="alert">{{mensagem}}</div>
-                        <div class="alert alert-success form-control" v-show="mensagemSuc!=''" role="alert">{{mensagemSuc}}</div>
-                        <p v-if="errors.length">
-                            <ul v-for="(error, index) in errors" v-bind:key="index">
-                                <li class="alert alert-danger form-control" >{{ error }}</li>
-                            </ul>
-                        </p>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label>
-                                    <b>Nome </b>
-                                </label>
-                                <input type="text" placeholder="Ex: Estanho" v-model="produto.productName" id="nome" class="form-control form-control-sm">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label>
-                                    <b>Descrição </b>
-                                </label>
-                                <input type="text" id="desc" class="form-control form-control-sm" v-model="produto.productDescription" placeholder="Ex: Estanho Metálico">
-                            </div>
+        <!--               Modal             -->        
+        <b-modal ref="myModalRef" hide-footer title="Cadastro de Matéria-Prima">                    
+            <form>
+                <div>
+                    <div class="alert alert-danger form-control" v-show="mensagem!=''" role="alert">{{mensagem}}</div>
+                    <div class="alert alert-success form-control" v-show="mensagemSuc!=''" role="alert">{{mensagemSuc}}</div>
+                    <p v-if="errors.length">
+                        <ul v-for="(error, index) in errors" v-bind:key="index">
+                            <li class="alert alert-danger form-control" >{{ error }}</li>
+                        </ul>
+                    </p>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label>
+                                <b>Nome </b>
+                            </label>
+                            <input type="text" placeholder="Ex: Estanho" v-model="produto.productName" id="nome" class="form-control form-control-sm">
                         </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label>
-                                    <b>Código </b>
-                                </label>
-                                <input class="form-control form-control-sm" type="text" v-model="produto.productCode" placeholder="Ex: 1010144" id="cod">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label>
-                                    <b>Código de barras </b>
-                                </label>
-                                <input type="text" id="gs1" v-model="produto.productGTIN" class="form-control form-control-sm" placeholder="Ex: 941120000000">
-                                <br>
-                            </div>
-                        </div>
-                        <div class="modal-footer">                            
-                            <div class="btn-group" role="group">
-                                <button @click.stop.prevent="alerta()?((produto.productId!=undefined) ? put(produto) : cadastrar(produto)):0" class="btn btn-success pull-right" :disabled="produto.productName==undefined || produto.productName=='' || produto.productCode=='' || produto.productGTIN==''">
-                                    <i  class="fa fa-check-square" aria-hidden="true"></i>
-                                </button>                    
-                            </div>
+                        <div class="form-group col-md-6">
+                            <label>
+                                <b>Descrição </b>
+                            </label>
+                            <input type="text" id="desc" class="form-control form-control-sm" v-model="produto.productDescription" placeholder="Ex: Estanho Metálico">
                         </div>
                     </div>
-                </form>
-            </b-modal>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label>
+                                <b>Código </b>
+                            </label>
+                            <input class="form-control form-control-sm" type="text" v-model="produto.productCode" placeholder="Ex: 1010144" id="cod">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label>
+                                <b>Código de barras </b>
+                            </label>
+                            <input type="text" id="gs1" v-model="produto.productGTIN" class="form-control form-control-sm" placeholder="Ex: 941120000000">
+                            <br>
+                        </div>
+                    </div>
+                    <div class="modal-footer">                            
+                        <div class="btn-group" role="group">
+                            <button @click.stop.prevent="alerta()?((produto.productId!=undefined) ? put(produto) : cadastrar(produto)):0" class="btn btn-success pull-right" :disabled="produto.productName==undefined || produto.productName=='' || produto.productCode=='' || produto.productGTIN==''">
+                                <i  class="fa fa-check-square" aria-hidden="true"></i>
+                            </button>                    
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </b-modal>
 
         <!--                       -->
         <!--                       -->
@@ -90,7 +88,7 @@
                     <button type="button" class="btn btn-success btn-lg" @click.stop.prevent="showModal"><i class="fa fa-plus" aria-hidden="true" ></i> Nova Materia Prima</button>
                 </li>
             </ul>                             
-        </div>
+        </div>        
         <div class="fundo-branco">                             
             <div class="cabecalho-table">
                 <label @click.stop.prevent="cabecalhoSetas[0]==false?desorganizar(produtos, 'productName',0):organizar(produtos, 'productName',0);" class="ls2-cabecalhotab">
