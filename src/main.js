bf84066ec7aadd78618817f472063fa63bb92ca4
fernandat <fefedel9@hotmail.com>
2018-04-12 15:05:50 -0300

    scroll report

--- src/main.js
+++ src/main.js
@@ -6,16 +6,24 @@ import Bootstrap from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 Vue.use({
-    install: function(Vue, options){
-        Vue.prototype.$jQuery = require('jquery'); 
-        }
+    install: function(Vue, options) {
+        Vue.prototype.$jQuery = require('jquery');
+    }
 })
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'amcharts3/amcharts/plugins/export/export.css'
+import 'amcharts3/amcharts/plugins/export/export.min.js'
+import 'amcharts3/amcharts/plugins/export/libs/pdfmake/pdfmake.js'
+
+
+// import 'amcharts3/plugins/export/libs/export.css'
 
 import popper from '.././node_modules/popper.js/dist/umd/popper.js'
 import icones from '.././node_modules/font-awesome/css/font-awesome.min.css'
+// import '.././node_modules/amcharts3/amcharts/plugins/export/export.min.js'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
