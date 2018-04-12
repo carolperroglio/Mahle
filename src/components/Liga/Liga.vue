ea8d31ec7c5458efbc7ac79f27539a74e13d1cfd
Ricardo Vieira Amorim dos Santos <ricardo.santos@integradora.com.br>
2018-04-12 08:34:08 -0300

    Pequenas mudancas responsividade e delete de lixo

--- src/components/Liga/Liga.vue
+++ src/components/Liga/Liga.vue
@@ -28,25 +28,27 @@
                     <b>Produto <br>Final</b>
                 </li>                 
                 <li class="nav-items-liga form-group col-sm-2"><!--recipe.recipeProduct.product.productName-->
-                    
-                    <input placeholder="Nome do produto" class="form-control form-control-sm" list="dropdownMenuButton" v-model="productRecipeName"
-                        @keyup="prosFim=getResults(urlProducts, productRecipeName, prosFim);delete recipeProduct.productId;">                                                           
-                    <datalist id="dropdownMenuButton" v-show="prosFim.length>0">    
-                        <option v-for="(p,i) in prosFim" :key="i"                        
-                        :value="p.productName" :label="p.productName">{{p.productName}}</option>
-                    </datalist>                        
+                    <input placeholder="Nome do produto" class="form-control form-control-sm" list="dropdownMenuButton" 
+                        v-model="productRecipeName" @keyup="prosFim=getResults(urlProducts, productRecipeName, prosFim); recipeProduct={}">                                                           
 
-                    <!--<b-dropdown-item id="dropdownMenuButton" 
-                        @click.stop.prevent="recipeProduct.productId=p.productId; recipeProduct.product=p; productRecipeName=p.productName; prosFim=[];" 
+                        <!--<select id="dropdownMenuButton" v-model="recipeProduct" class="form-control form-control-sm"
+                            style="position:absolute;" v-show="prosFim.length>0">                                
+                            <option v-for="(p,i) in prosFim" :key="i"                        
+                            :value="p">{{p.productName}}</option>
+                        </select> -->                       
+
+                    <b-dropdown-item id="dropdownMenuButton" style="position:absolute !important; background-color:white;" class="form-control form-control-sm"
+                        @click.stop.prevent="recipeProduct.product=p; productRecipeName=p.productName; prosFim=[];" 
                         v-for="(p,index) in prosFim" v-bind:key="index">
                         {{p.productName}}                    
-                    </b-dropdown-item>-->
+                    </b-dropdown-item>
                 </li>
                 <li class="nav-items-liga col-sm-2" v-if="!recipeCadastrada" >                    
-                    <button type="button" class="btn btn-success btn-sm" :disabled="!recipe.recipeName || !recipe.recipeCode || recipe.recipeCode==undefined || recipe.recipeName=='' || recipe.recipeCode==''"  @click.stop.prevent="createRecipe(recipe)">
+                    <button type="button" class="btn btn-success btn-sm" :disabled="!recipe.recipeName || !recipe.recipeCode || !recipeProduct || recipeProduct.product==undefined || recipe.recipeCode==undefined || recipe.recipeName=='' || recipe.recipeCode==''"  @click.stop.prevent="createRecipe(recipe,recipeProduct)">
                         Enviar
-                    </button>                    
+                    </button>   
                 </li> 
+                {{p}}
                 <li class="nav-items-liga col-sm-0" v-if="recipeCadastrada">
                     <button class="btn btn-warning btn-sm" @click.stop.prevent="showModalEditRecipe(recipe)" >                        
                         <i class="fa fa-pencil" style="font-size:22px; cursor:pointer"></i>                          
